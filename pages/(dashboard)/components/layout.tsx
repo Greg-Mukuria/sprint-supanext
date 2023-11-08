cfc1e795b621a3c13f046b8a5b4834d810d5f75e
Greg <103807051+Greg-Mukuria@users.noreply.github.com>
2023-11-08 20:54:16 +0300

    Create layout.tsx

---
+++ pages/(dashboard)/components/layout.tsx
@@ -0,0 +1,11 @@
+import Header from './header';
+import Submenu from './submenu';
+import UserSettings from './user-settings';
+
+export default function Layout() {
+  return(
+    <Header />
+    <Submenu />
+    <UserSettings />
+  )
+};
